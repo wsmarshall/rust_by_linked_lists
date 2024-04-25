ec0ef223553d8693396f1c897b848db61e6c2e00
Warren <42559252+wsmarshall@users.noreply.github.com>
2024-04-25 15:45:02 -0700

    impls IntoIter as wrapper for List, tests pass

--- src/second.rs
+++ src/second.rs
@@ -1,3 +1,21 @@
+//a "tuple struct" as a trivial wrapper around
+//the List type
+pub struct IntoIter<T>(List<T>);
+
+impl<T> List<T> {
+    pub fn into_iter(self) -> IntoIter<T> {
+        IntoIter(self)
+    }
+}
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item> {
+        //access fields of a tuple struct numerically
+        self.0.pop()
+    }
+}
+
 pub struct List<T> {
     head: Link<T>,
 }
@@ -55,48 +73,16 @@ mod test {
     use super::List;
 
     #[test]
-    fn basics() {
+    fn into_iter() {
         let mut list = List::new();
-
-        //check empty list behaves correctly
-        assert_eq!(list.pop(), None);
-
-        //populate list
         list.push(1);
         list.push(2);
         list.push(3);
 
-        //check normal removal
-        assert_eq!(list.pop(), Some(3));
-        assert_eq!(list.pop(), Some(2));
-
-        //push more
-        list.push(4);
-        list.push(5);
-
-        //check normal removal
-        assert_eq!(list.pop(), Some(5));
-        assert_eq!(list.pop(), Some(4));
-
-        //check exhaustion
-        assert_eq!(list.pop(), Some(1));
-        assert_eq!(list.pop(), None);
-    }
-
-    #[test]
-    fn peek() {
-        let mut list = List::new();
-        assert_eq!(list.peek(), None);
-        assert_eq!(list.peek_mut(), None);
-        list.push(1);
-        list.push(2);
-        list.push(3);
-
-        assert_eq!(list.peek(), Some(&3));
-        assert_eq!(list.peek_mut(), Some(&mut 3));
-        list.peek_mut().map(|value| *value = 42);
-
-        assert_eq!(list.peek(), Some(&42));
-        assert_eq!(list.pop(), Some(42));
+        let mut iter = list.into_iter();
+        assert_eq!(iter.next(), Some(3));
+        assert_eq!(iter.next(), Some(2));
+        assert_eq!(iter.next(), Some(1));
+        assert_eq!(iter.next(), None);
     }
 }
